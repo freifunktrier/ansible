16ebb0a4ccf950d5b604664bd217115167be7515
Ranlvor <ranlvor@starletp9.de>
2015-12-22 15:47:01 +0100

    added -P 60 ad parameter to mkbgp
    This causes all BGP-Links that do not TCP-connect within 60 seconds to be configured as passive peer

--- roles/icvpn-autoupdate/files/update-icvpnmeta.sh
+++ roles/icvpn-autoupdate/files/update-icvpnmeta.sh
@@ -2,11 +2,11 @@
 cd /var/lib/icvpn/icvpn-meta
 git pull
 
-sudo -u nobody /var/lib/icvpn/icvpn-scripts/mkbgp -4 -f bird -d peers -s /var/lib/icvpn/icvpn-meta -x trier -t berlin:uplink_peers | sed 's/protocol bgp berlin2 from uplink_peers {/protocol bgp berlin2 from prefered_uplink_peers {/' > /etc/bird/bird.d/icvpn.conf
+sudo -u nobody /var/lib/icvpn/icvpn-scripts/mkbgp -P 60 -4 -f bird -d peers -s /var/lib/icvpn/icvpn-meta -x trier -t berlin:uplink_peers | sed 's/protocol bgp berlin2 from uplink_peers {/protocol bgp berlin2 from prefered_uplink_peers {/' > /etc/bird/bird.d/icvpn.conf
 sudo -u nobody /var/lib/icvpn/icvpn-scripts/mkroa -4 -f bird -s /var/lib/icvpn/icvpn-meta > /etc/bird/roa4.con
 birdc configure
 
-sudo -u nobody /var/lib/icvpn/icvpn-scripts/mkbgp -6 -f bird -d peers -s /var/lib/icvpn/icvpn-meta -x trier -t berlin:uplink_peers | sed 's/protocol bgp berlin2 from uplink_peers {/protocol bgp berlin2 from prefered_uplink_peers {/' > /etc/bird/bird6.d/icvpn.conf
+sudo -u nobody /var/lib/icvpn/icvpn-scripts/mkbgp -P 60 -6 -f bird -d peers -s /var/lib/icvpn/icvpn-meta -x trier -t berlin:uplink_peers | sed 's/protocol bgp berlin2 from uplink_peers {/protocol bgp berlin2 from prefered_uplink_peers {/' > /etc/bird/bird6.d/icvpn.conf
 sudo -u nobody /var/lib/icvpn/icvpn-scripts/mkroa -6 -f bird -s /var/lib/icvpn/icvpn-meta > /etc/bird/roa6.con
 birdc6 configure
 
