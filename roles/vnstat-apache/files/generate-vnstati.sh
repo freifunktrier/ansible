7cb6c255d9961e4294f787732cd6aebaf5c34394
tackin <h.schmitz@digital-layout.de>
2017-09-08 09:44:56 +0200

    update iface names

--- roles/vnstat-apache/files/generate-vnstati.sh
+++ roles/vnstat-apache/files/generate-vnstati.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /var/www/html/traffic
 for i in t m s; do
-        for j in fftr-mesh-vpn icvpn; do
+        for j in fvpn fvpn_01 fvpn_02 fvpn_03 fvpn_04 fvpn_05 icvpn; do
                 vnstati -$i -i $j -o vnstati-$i-$j.png
         done
 done
