325b0257deb9884cca684a6cbba0df51d1a944f5
tackin <tackin@mail2.digital-layout.de>
2018-02-09 00:40:41 +0100

    add iftop, add figures of eth0 and eth1  ... just in case the ifaces exists

--- roles/vnstat-apache/files/generate-vnstati.sh
+++ roles/vnstat-apache/files/generate-vnstati.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /var/www/html/traffic
 for i in t m s; do
-        for j in fvpn fvpn_01 fvpn_02 fvpn_03 fvpn_04 fvpn_05 icvpn ens3; do
+        for j in fvpn fvpn_01 fvpn_02 fvpn_03 fvpn_04 fvpn_05 icvpn ens3 eth0 eth1; do
                 vnstati -$i -i $j -o vnstati-$i-$j.png
         done
 done
