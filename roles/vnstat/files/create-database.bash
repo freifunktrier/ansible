b80bcdf701f42e942314f1c52a12b1a35a6ad92e
tackin <tackin@mail2.digital-layout.de>
2020-02-06 17:50:04 +0100

    some changes needed for now

--- roles/vnstat/files/create-database.bash
+++ roles/vnstat/files/create-database.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in $(vnstat --iflist | sed "s/Available interfaces: //"); do
+for i in $(vnstat --iflist | sed "s/Available interfaces: //" |  sed -e "s/([^)]*)//g"); do
         echo "creating $i"
         sudo -u vnstat vnstat -u -i $i
 done
